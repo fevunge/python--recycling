d4392f4f26395489c04cdfe0f9ac4a4be61b878a
fevunge <fevunge@outlook.com>
2025-09-02 09:50:11 +0100

    fix: correct input handling for calculation when no arguments are provided

--- calc.py
+++ calc.py
@@ -1,10 +1,10 @@
 import sys
 
 express = ' '.join(sys.argv[1:])
-
-if len(sys.argv) < 1:
-    args = input("enter calc, this way (n1 '+' n2):: ")
+if len(sys.argv[1:]) < 1:
+    express = input("enter calc, this way (n1 '+' n2):: ")
 args = express.split(' ')
+print(args)
 func = None
 
 result = 0
