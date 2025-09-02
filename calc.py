3fa4d7b83f36ac6d0bd0b0db2a457473fd7b6ed4
fevunge <fevunge@outlook.com>
2025-09-02 11:37:48 +0100

    chore: remove debug print statement from calculator script

--- calc.py
+++ calc.py
@@ -4,7 +4,6 @@ express = ' '.join(sys.argv[1:])
 if len(sys.argv[1:]) < 1:
     express = input("enter calc, this way (n1 '+' n2):: ")
 args = express.split(' ')
-print(args)
 func = None
 
 result = 0
